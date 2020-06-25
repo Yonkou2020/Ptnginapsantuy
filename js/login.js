5754ffed636bc606d4f3715156abd8b6f4681bd5
Ciputra Pangestu <ciputrapangestu2810@gmail.com>
2020-06-25 08:13:31 +0200

    add border and login page

--- js/login.js
+++ js/login.js
@@ -1,11 +1,45 @@
 let button = document.querySelector('.button');
 
-button.addEventListener('click', function(){
+button.addEventListener('click', postData)
+
+
+async function postData() {
+    try {
+        let fullName = document.querySelector('#name').value;
+        let email = document.querySelector('#email').value;
+        let passWord = document.querySelector('#password').value;
+
+        let users = {
+            fullName,
+            email,
+            passWord
+        }
+
+        let url = 'https://5ef168f21faf160016b4d5c9.mockapi.io/api/users';
+        let options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(users)
+        }
+        let response = await fetch(url, options);
+        let result = await response.json();
+        getAlert()
+        
+        console.log(result)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+ 
+function getAlert(){
     Swal.fire({
-        title: 'Your Email Has Been Registered !',
-        text: 'Thank You',
-        icon: 'success',
-        confirmButtonText: 'Continue'
-      })
-})
+    title: 'Your Email Has Been Registered !',
+    text: 'Thank You',
+    icon: 'success',
+    confirmButtonText: 'Continue'
+  })
+}
 
