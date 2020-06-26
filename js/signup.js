9aff7f1c6baedcc9e7f1eaa1c29a7d5e49d932bf
Ciputra Pangestu <ciputrapangestu2810@gmail.com>
2020-06-26 09:44:53 +0200

    add verification to signup html

--- js/signup.js
+++ js/signup.js
@@ -14,28 +14,55 @@ async function postData() {
             email,
             passWord
         }
+    
+            let url = 'https://5ef168f21faf160016b4d5c9.mockapi.io/api/users';
+    
+            let response = await fetch(url);
+            let result = await response.json();
+            console.log(result)
+    
+    
+            let registeredUsers = result.filter(arr =>{
+                return arr.email === email
+                
+            })
+            console.log(registeredUsers)
+            if(registeredUsers.length > 0){
+                Swal.fire({
+                    title: 'You Already Registered Before',
+                    text: `Log in to your Email`,
+                    icon: 'success',
+                    confirmButtonText: 'Continue'
+                  })
+                  setTimeout(function(){
+                    location.replace('./login.html')
+                }, 4000);
+                }
+            else{
 
-        let url = 'https://5ef168f21faf160016b4d5c9.mockapi.io/api/users';
-        let options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(users)
+                let options = {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(users)
+                }
+                let response = await fetch(url, options);
+                let result = await response.json();
+                getAlert()
+                setTimeout(function(){
+                    location.replace('./index.html')
+                }, 4000);
+                
+    
+                   
+                     }
         }
-        let response = await fetch(url, options);
-        let result = await response.json();
-        getAlert()
-        setTimeout(function(){
-            location.replace('./index.html')
-        }, 4000);
-        
-        
-        console.log(result)
-    } catch (error) {
-        console.error(error)
+        catch(error){
+            console.error(error)
+        }
     }
-}
+       
 
  
 function getAlert(){
