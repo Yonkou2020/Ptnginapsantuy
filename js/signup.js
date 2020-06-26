6c9c25f65b4bb49daa2f38aae7099551932aa982
agusTrih <ant3dong@gmail.com>
2020-06-27 01:51:20 +0700

    add fitur sign up feat putra

--- js/signup.js
+++ js/signup.js
@@ -1,49 +1,65 @@
-let button = document.querySelector('.button');
+let button = document.querySelector(".button");
 
-button.addEventListener('click', postData)
-
+button.addEventListener("click", postData);
 
 async function postData() {
     try {
-        let fullName = document.querySelector('#name').value;
-        let email = document.querySelector('#email').value;
-        let passWord = document.querySelector('#password').value;
+        let fullName = document.querySelector("#name").value;
+        let email = document.querySelector("#email").value;
+        let passWord = document.querySelector("#password").value;
 
         let users = {
             fullName,
             email,
-            passWord
-        }
+            passWord,
+        };
+        // tambahan dari putra
+        let url = "https://5ef168f21faf160016b4d5c9.mockapi.io/api/users";
 
-        let url = 'https://5ef168f21faf160016b4d5c9.mockapi.io/api/users';
-        let options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(users)
-        }
-        let response = await fetch(url, options);
+        let response = await fetch(url);
         let result = await response.json();
-        getAlert()
-        setTimeout(function(){
-            location.replace('./index-welcome.html')
-        }, 4000);
-        
-        
-        console.log(result)
+        console.log(result);
+
+        let registeredUsers = result.filter((arr) => {
+            return arr.email === email;
+        });
+        console.log(registeredUsers);
+        if (registeredUsers.length > 0) {
+            Swal.fire({
+                title: "You Already Registered Before",
+                text: `Log in to your Email`,
+                icon: "success",
+                confirmButtonText: "Continue",
+            });
+            setTimeout(function () {
+                location.replace("./login.html");
+            }, 4000);
+        } else {
+            // tambahan kedua
+            let options = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(users),
+            };
+            let response = await fetch(url, options);
+            let result = await response.json();
+            getAlert();
+            setTimeout(function () {
+                location.replace("./index.html");
+            }, 4000);
+        }
     } catch (error) {
-        console.error(error)
+        console.error(error);
     }
 }
 
- 
-function getAlert(){
+function getAlert() {
     Swal.fire({
-    title: 'Your Email Has Been Registered !',
-    text: 'Thank You',
-    icon: 'success',
-    confirmButtonText: 'Continue'
-  })
+        title: "Your Email Has Been Registered !",
+        text: "Thank You",
+        icon: "success",
+        confirmButtonText: "Continue",
+    });
 }
-
