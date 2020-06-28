80c9c2d4d5dc5bb5d7ecf647bbba28e68d88709a
agusTrih <ant3dong@gmail.com>
2020-06-29 06:28:23 +0700

    preaper to add master

--- checkout.js
+++ checkout.js
@@ -1,16 +1,16 @@
-let btnCheckout = document.getElementById('checkout');
-btnCheckout.addEventListener('click', checkout)
+let btnCheckout = document.getElementById("checkout");
+btnCheckout.addEventListener("click", checkout);
 
-function checkout(){
+function checkout() {
     setTimeout(() => {
-        location.replace('./checkout.html');
-     }, 500);  
+        location.replace("./checkout.html");
+    }, 500);
 }
 
-let btnBackToVilla = document.getElementById('back-to-villa');
-btnBackToVilla.addEventListener('click', backToVilla)
-function backToVilla(){
+let btnBackToVilla = document.getElementById("back-to-villa");
+btnBackToVilla.addEventListener("click", backToVilla);
+function backToVilla() {
     setTimeout(() => {
-        location.replace('./list-villa.html');
-     }, 500);  
+        location.replace("./list-villa.html");
+    }, 500);
 }
